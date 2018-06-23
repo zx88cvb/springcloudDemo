bee90e4dc8ca073f1b532564ce62bbf1cc7643bb
zx88cvb <295389754@qq.com>
2018-06-23 22:11:36 +0800

    bus

--- order/server/src/main/java/com.angel/controller/EnvController.java
+++ order/server/src/main/java/com.angel/controller/EnvController.java
@@ -1,6 +1,7 @@
 package com.angel.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/env")
+@RefreshScope   //自动加载配置刷新
 public class EnvController {
     @Value("${env}")
     private String env;
